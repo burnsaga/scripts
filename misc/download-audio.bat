35bcd5f51a239b9208bea6623129c41c697f2d01
Joe Burns <11431079+burnsaga@users.noreply.github.com>
2020-05-01 06:25:16 -0400

    Update download-audio.bat

--- misc/download-audio.bat
+++ misc/download-audio.bat
@@ -1,8 +1,8 @@
-@ECHO OFF
-TITLE Download Audio (YouTube/Soundcloud, best quality)
+@echo off
+title Download Audio (YouTube/Soundcloud, best quality)
 :: Prompt user for URL
 :a
-SET /p url=Enter the URL to download: 
+set /p url=Enter the URL to download: 
 :: Call youtube-dl to download audio in the best quality available
 youtube-dl %url% -f bestaudio
-GOTO a
+goto a
