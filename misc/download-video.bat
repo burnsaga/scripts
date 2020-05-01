7849286ac66b91bfa48152d91da62503c0391d9e
Joe Burns <11431079+burnsaga@users.noreply.github.com>
2020-05-01 06:25:19 -0400

    Update download-video.bat

--- misc/download-video.bat
+++ misc/download-video.bat
@@ -1,8 +1,8 @@
-@ECHO OFF
-TITLE Download Video (YouTube, best quality)
+@echo off
+title Download Video (YouTube, best quality)
 :: Prompt user for URL
 :a
-SET /p url=Enter the URL to download: 
+set /p url=Enter the URL to download: 
 :: Call youtube-dl to download YouTube video
 youtube-dl %url%
-GOTO a
+goto a
