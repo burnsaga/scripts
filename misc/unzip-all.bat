40e719fc5ba82ccb0c0a3c828af9927ca3c1dfae
Joe Burns <11431079+burnsaga@users.noreply.github.com>
2020-05-01 06:20:33 -0400

    Update unzip-all.bat

--- misc/unzip-all.bat
+++ misc/unzip-all.bat
@@ -1,11 +1,11 @@
-TITLE Unzip All (.zip, .7z, .rar)
+title Unzip All (.zip, .7z, .rar)
 :: Recursively extract from archive files with 7Zip to the same folder the archive file is located (.zip, .7z, .rar)
 :: ref: https://superuser.com/questions/1224218/batch-extract-files-from-every-archive-with-7-zip
 
-@ECHO ON
+@echo off
 
-FOR /F "TOKENS=*" %%F IN ('DIR /S /B "*.zip"') DO "C:\Program Files\7-Zip\7z.exe" x "%%~fF" -o"%%~pF\"
-FOR /F "TOKENS=*" %%F IN ('DIR /S /B "*.7z"') DO "C:\Program Files\7-Zip\7z.exe" x "%%~fF" -o"%%~pF\"
-FOR /F "TOKENS=*" %%F IN ('DIR /S /B "*.rar"') DO "C:\Program Files\7-Zip\7z.exe" x "%%~fF" -o"%%~pF\"
+for /F "TOKENS=*" %%F in ('DIR /S /B "*.zip"') do "C:\Program Files\7-Zip\7z.exe" x "%%~fF" -o"%%~pF\"
+for /F "TOKENS=*" %%F in ('DIR /S /B "*.7z"') do "C:\Program Files\7-Zip\7z.exe" x "%%~fF" -o"%%~pF\"
+for /F "TOKENS=*" %%F in ('DIR /S /B "*.rar"') do "C:\Program Files\7-Zip\7z.exe" x "%%~fF" -o"%%~pF\"
 
-EXIT
+exit
