025c48c85032933644335e859926adbb2a1673a9
Joe Burns <joeburns@protonmail.com>
2020-12-13 23:12:19 -0500

    Create av-trim-mp4-lossless-trim-from-a-to-b.bat

---
+++ misc/av-trim-mp4-lossless-trim-from-a-to-b.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+TITLE Trim from A to B (from a file 'input.mp4')
+:: ref: https://www.arj.no/2018/05/18/trimvideo/
+
+:: Prompt user
+SET /p trim_start=Enter the start time to trim from (hh:mm:ss e.g. 00:01:00): 
+SET /p trim_end=Enter the end time to trim from (hh:mm:ss):
+
+:: Run script and create output.mp4
+ffmpeg -i input.mp4 -ss %trim_start% -to %trim_end% -c:v copy -c:a copy output.mp4
